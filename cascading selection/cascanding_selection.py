b35ece56b8e2110d4d74a588032b4025fea817a8
Tüzes Dániel otthonról <tuzesdaniel@gmail.com>
2021-09-20 18:39:49 +0100

    separate the functions from main: provide UI

--- cascading selection/cascanding_selection.py
+++ cascading selection/cascanding_selection.py
@@ -1,12 +1,13 @@
 """Filter and select data to which values are assigned."""
 
-from typing import Hashable, List
+from typing import List
+import argparse
 import pandas as pd
 
 
 def cascading_assignment(data: pd.DataFrame,
                          sel_r: pd.DataFrame,
-                         priority: List[Hashable]) -> pd.DataFrame:
+                         priority: List[str]) -> pd.DataFrame:
     """Assigns values to the data based on the cascading selection rules.
 
     Parameters
@@ -16,7 +17,7 @@ def cascading_assignment(data: pd.DataFrame,
         and to which further properties should be added.
     sel_r : pd.DataFrame
         [description]
-    priority : List[Hashable]
+    priority : List[str]
         [description]
 
     Returns
@@ -46,12 +47,12 @@ def cascading_assignment(data: pd.DataFrame,
                           how="inner",
                           on=selectors)
         ret.update(merged.set_index(data.index.name)[val_n])
-    ret.set_index(data.index.name)
+    ret.set_index(data.index.name, inplace=True)
     return ret
 
 
 def _calc_scores(m_rule: pd.DataFrame, priority) -> None:
-    """Calculate the scores and list the selectors."""
+    """Assign the priority scores to the selection rules."""
 
     scores = {item: 2**(len(priority)-i-1) for i, item in enumerate(priority)}
     sns = pd.DataFrame(index=m_rule.index)
@@ -65,19 +66,46 @@ def _calc_scores(m_rule: pd.DataFrame, priority) -> None:
     return sns
 
 
-dat = pd.read_csv("people.csv")
-dat.index.name = "pd index"
-sel_rule = pd.read_csv("selection_rules.csv",
-                       dtype={"personal ID": "Int64",
-                              "family name": str,
-                              "given name": str,
-                              "fav color": str,
-                              "visitor": bool,
-                              "hall": str,
-                              "buffet": str})
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=('Assign properties to '
+                                                  'entries based on '
+                                                  'cascading selection '
+                                                  'rules.'))
+    parser.add_argument('data_fname',
+                        metavar='data.csv',
+                        help=('The filename that contains the data to which'
+                              ' new properties need to be assigned.'))
+    parser.add_argument('sel_prop_fname',
+                        metavar='rules.csv',
+                        help=('The filename that contains the selection rules '
+                              'and the properties that are needed to be '
+                              'assigned to the data. The columns must be '
+                              'ordered in a decreasing priority order, '
+                              'followed by the columns of the properties.'))
+    parser.add_argument('nof_sel_col',
+                        metavar="N",
+                        help=('The number of columns containing the selection '
+                              'rules.'),
+                        type=int)
+    parser.add_argument('--output_fname', '-o',
+                        metavar='data_with_properties.csv',
+                        help=('If given, the output is written '
+                              'to this file in csv format '
+                              'instead of the stdout in padded text format.'))
+    args = parser.parse_args()
 
-priori = ["personal ID", "family name", "given name", "fav color", "visitor"]
+    dat = pd.read_csv(args.data_fname)
+    dat.index.name = "df index"  # it must not be None
+    dat.add_prefix("_")  # add prefix so name collision can be precented
+    sel_rule = pd.read_csv(args.sel_prop_fname)
+    priori = sel_rule.columns[:args.nof_sel_col]
 
-result = cascading_assignment(dat, sel_rule, priori)
+    result = cascading_assignment(dat, sel_rule, priori)
 
-print("Done")
+    # remove what has been added
+    dat.columns = dat.columns.str.replace(r'^_', '', regex=True)
+
+    if args.output_fname is not None:
+        result.to_csv(args.output_fname, index=False)
+    else:
+        print(result.to_string(index=False))
