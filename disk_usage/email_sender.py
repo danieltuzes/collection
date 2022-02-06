ef5e834d8eb7267ba62782a4d9510e178a3ead6b
Tüzes Dániel otthonról <tuzesdaniel@gmail.com>
2022-02-06 21:05:04 +0000

    correct email sender

--- disk_usage/email_sender.py
+++ disk_usage/email_sender.py
@@ -1,8 +1,9 @@
 """Notify users if they use a lot of space."""
 
 import sys
-import time
-import subprocess
+import smtplib
+from email.message import EmailMessage
+from typing import Dict, Tuple
 
 # region settings
 TB_3_5 = 3.5 * 1024 * 1024 * 1024
@@ -11,7 +12,7 @@ SERVER_CONF = {"disk_usage1.html": ("first", "http://first:5000", TB_3_5),
                "disk_usage2.html": ("second", "http://second:5000", TB_3_5)}
 
 
-# notify the user if uses this ratio of the total space
+# notify the user if uses this ratio of the total space, between [0,1]
 USER_TRIGGER = 0.0
 
 # notify the user if the used disk size is larger than this ratio
@@ -21,14 +22,20 @@ EMAIL_DOMAIN = "@gmail.com"
 
 # overwrite default e-mails as username@domain
 EMAIL_ADDR = {"tdani0": "tuzesdaniel+tdani0@gmail.com",
-              "danit1": "danieltuzes+danit1@gmail.com"}
+              "danit1": "danieltuzes+danit1@gmail.com",
+              "tdannni": "tuzesdaniel+tdanni@gmail.com",
+              "tuzesdanit1": "tuzesdaniel+tdanni@gmail.com",
+              "danieldanit1": "tuzesdaniel+tdanni@gmail.com",
+              "tdani0danit1": "tuzesdaniel+tdanni@gmail.com",
+              "danit1danit1": "tuzesdaniel+tdanni@gmail.com"}
 
-METHOD = ["console"]  # ["email", "console"]
+METHOD = ["console", "email"]  # ["email", "console"]
+SENDER = "tuzesdaniel@gmail.com"
 
 # endregion
 
 
-def get_disk_usage(du_fname: str) -> dict[str, int]:
+def get_disk_usage(du_fname: str) -> Dict[str, int]:
     """Reads in the disk usage file"""
     with open(du_fname, mode="r", encoding="utf-8") as ifile:
         lines = ifile.read().split("\n")[1:-2]
@@ -37,8 +44,8 @@ def get_disk_usage(du_fname: str) -> dict[str, int]:
     return usage
 
 
-def notify_users(usage: dict[str, int],
-                 server_conf: tuple[str, str]) -> None:
+def notify_users(usage: Dict[str, int],
+                 server_conf: Tuple[str, str]) -> None:
     """Send email or sprint to screen."""
     for user, disk_u in usage.items():
         p_ratio = disk_u/server_conf[2]  # personal ratio
@@ -47,29 +54,41 @@ def notify_users(usage: dict[str, int],
 
 
 def notify(piv_user: str,
-           usage: dict[str, int],
-           server_conf: tuple[str, str, float]) -> None:
+           usage: Dict[str, int],
+           server_conf: Tuple[str, str, float]) -> None:
     """Notify a specific user."""
-    message = ("You've got this mail because the server disk usage on "
-               f"'{server_conf[0]}' has reached {SPACE_TRIGGER*100}% and you "
-               f"consume more than {USER_TRIGGER*100}% of this space. "
-               "List of users and their usage:\n"
-               "       ID\tusage [%]\n")
+    email_text = EmailMessage()
+    email_text["From"] = SENDER
+
+    rec_addr = EMAIL_ADDR.get(piv_user, f"{piv_user}{EMAIL_DOMAIN}")
+
+    email_text["To"] = rec_addr
+    email_text["Subject"] = "Disk usage notification"
+
+    body = ("You've got this mail because the server disk usage on "
+            f"'{server_conf[0]}' has reached {SPACE_TRIGGER*100}% and you "
+            f"consume more than {USER_TRIGGER*100}% of this space. "
+            "List of users and their usage:\n<pre>"
+            "       ID\tusage [%]\n")
     for user, disk_u in usage.items():
         perc = int(disk_u/server_conf[2]*100)
         if user == piv_user:
-            message += f"you -> {user}\t{perc}\n"
+            body += f"you -> {user}\t{perc}\n"
         else:
-            message += f"       {user}\t{perc}\n"
-    message += (f"\nVisit {server_conf[1]} for more info.")
+            body += f"       {user}\t{perc}\n"
+    body += (f"\n</pre>Visit {server_conf[1]} for more info.")
 
-    email = f"{piv_user}{EMAIL_DOMAIN}"
-    syscall = ["echo", "-e", message, "|", "sendmail", email]
+    email_text.set_content(body, subtype='html')
 
     if "email" in METHOD:
-        subprocess.run(syscall, check=False)
+        try:
+            smtp_obj = smtplib.SMTP('localhost')
+            smtp_obj.send_message(email_text)
+            print("Successfully sent email")
+        except smtplib.SMTPException:
+            print("Error: unable to send email")
     if "console" in METHOD:
-        print(syscall)
+        print(email_text)
 
 
 def main() -> None:
@@ -79,7 +98,6 @@ def main() -> None:
         ratio = sum(usage.values()) / server_conf[2]
         if ratio > SPACE_TRIGGER:
             notify_users(usage, server_conf)
-        time.sleep(1)
 
 
 if __name__ == "__main__":
